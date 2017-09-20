4872c1f9280d407c989927aba9ad3bfb2d6517e5
ifinney <ifinney@dons.usfca.edu>
2017-09-20 01:12:32 +0000

    Add initial comments to fasta_QC_checks.shscript

---
+++ fasta_QC_checks.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# This is bashscript for Week 4 homework assignment 
+# This script will help in exploring and applying to
+# different applications tmux, ssh, and git have to offer 
+
+#The first portion of this script should output a log of some of the 
+#information pertaining to the zip files in data
+
+# A for loop hould help accomplish the second portion of this asignment which is
+# to output information about each zip file
+
+
+
+#cd data
+#unzip FastaSeqCL.fasta.zip
+#unzip H1N1_NS.fasta.zip
+#unzip crab_mitochondrial_rRNA.fasta.zip
+#unzip H1N1_HA.fasta.zip
+#unzip H1N1_PA.fasta.zip
+#unzip invertebrates_mRNA.fasta.zip
+#unzip H1N1_MP.fasta.zip
+#unzip H1N1_PB1.fasta.zip
+#unzip plant_rRNA.fasta.zip
+#unzip H1N1_NA.fasta.zip
+#unzip H1N1_PB2.fasta.zip
+#unzip H1N1_NP.fasta.zip
+#unzip animal_mRNA.fasta.zip
+
+echo "Unzipping fasta files"
+# This code will output a series of quality checks and information about the fasta files present in data
+
+
+
