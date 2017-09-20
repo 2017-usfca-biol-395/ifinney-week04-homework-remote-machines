84d05ceb9c9ed55c5db723d2df0c6d8814e273de
ifinney <ifinney@dons.usfca.edu>
2017-09-20 03:46:56 +0000

    Add commands to script to read lines and sequence number of fasta files

--- fasta_QC_checks.sh
+++ fasta_QC_checks.sh
@@ -9,23 +9,8 @@
 # A for loop hould help accomplish the second portion of this asignment which is
 # to output information about each zip file
 
-
-
-#cd data
-#unzip FastaSeqCL.fasta.zip
-#unzip H1N1_NS.fasta.zip
-#unzip crab_mitochondrial_rRNA.fasta.zip
-#unzip H1N1_HA.fasta.zip
-#unzip H1N1_PA.fasta.zip
-#unzip invertebrates_mRNA.fasta.zip
-#unzip H1N1_MP.fasta.zip
-#unzip H1N1_PB1.fasta.zip
-#unzip plant_rRNA.fasta.zip
-#unzip H1N1_NA.fasta.zip
-#unzip H1N1_PB2.fasta.zip
-#unzip H1N1_NP.fasta.zip
-#unzip animal_mRNA.fasta.zip
-
+unzip data/*.zip
+rm data/*.zip
 echo "Unzipping fasta files"
 # This code will output a series of quality checks and information about the fasta files present in data
 
@@ -55,7 +40,11 @@ do
 	echo "#######################################################"
 	echo "First three lines"
 	echo "-----------------"
-
-
+	head -3 $file
+	echo " "
+	echo "-----------------"
+	echo "Last three lines:"
+	tail -3 $file
+	echo "Number of sequences: $(grep -c "^" $file)"
 done
 
